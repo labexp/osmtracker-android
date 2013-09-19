f059d3f75c7afa560bba5ea6c0636a1bc914b5bf
thePanz <thepanz@gmail.com>
2013-09-19 13:54:36 +0200

    Fixes for numeric types (using Float instead of Double) Added getDistance() given two GeoPoints

--- src/me/guillaumin/android/osmtracker/util/DistanceUtil.java
+++ src/me/guillaumin/android/osmtracker/util/DistanceUtil.java
@@ -1,10 +1,12 @@
 package me.guillaumin.android.osmtracker.util;
 
+import org.osmdroid.api.IGeoPoint;
+
 public class DistanceUtil {
 
 	private static final double DEG2RAD = Math.PI / 180.0;
 	// private static final double RAD2DEG = 180.0 / Math.PI;
-	private static final double RADIUS_EARTH_METERS = 6378137.0;
+	private static final int RADIUS_EARTH_METERS = 6378137;
 	
 	/**
 	 * Computes the distance with Spherical Law of Cosines
@@ -15,12 +17,27 @@ public class DistanceUtil {
 	 * @param lon2
 	 * @return The distance (in meters) between point1 and point2
 	 */
-	public static double getDistance(final double lat1, final double lon1, final double lat2, final double lon2) {
-	  double theta = lon1 - lon2;
-	  double a = Math.sin(DEG2RAD * lat1) * Math.sin(DEG2RAD * lat2);
-	  double b = Math.cos(DEG2RAD * lat1) * Math.cos(DEG2RAD * lat2) * Math.cos(DEG2RAD * theta);
+	public static float getDistance(final float lat1, final float lon1, final float lat2, final float lon2) {
+	  final double theta = lon1 - lon2;
+	  final double a = Math.sin(DEG2RAD * lat1) * Math.sin(DEG2RAD * lat2);
+	  final double b = Math.cos(DEG2RAD * lat1) * Math.cos(DEG2RAD * lat2) * Math.cos(DEG2RAD * theta);
 	  
-	  return Math.acos(a + b) * RADIUS_EARTH_METERS;
+	  return (float) (Math.acos(a + b) * RADIUS_EARTH_METERS);
 	}
-
+	
+	/**
+	 * Computes the distance with Spherical Law of Cosines
+	 * 	http://www.movable-type.co.uk/scripts/latlong.html
+	 * @param point1 
+	 * @param point2
+	 * @return The distance (in meters) between point1 and point2
+	 */
+	public static float getDistance(final IGeoPoint point1, final IGeoPoint point2) {
+	  final float lat1 = (float) (point1.getLatitudeE6() / 1E6);
+	  final float lon1 = (float) (point1.getLongitudeE6() / 1E6);
+	  final float lat2 = (float) (point2.getLatitudeE6() / 1E6);
+	  final float lon2 = (float) (point2.getLongitudeE6() / 1E6);
+	  
+	  return getDistance(lat1, lon1, lat2, lon2);	
+	}
 }
