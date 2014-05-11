d47b987172ce8643c0efe95a2a55ede6fc6b7715
xtof <cerisara@gmail.com>
2014-05-11 17:43:11 +0200

    test

--- src/me/guillaumin/android/osmtracker/activity/WaypointList.java
+++ src/me/guillaumin/android/osmtracker/activity/WaypointList.java
@@ -1,11 +1,19 @@
 package me.guillaumin.android.osmtracker.activity;
 
+import java.io.File;
+
 import me.guillaumin.android.osmtracker.db.TrackContentProvider;
 import me.guillaumin.android.osmtracker.db.TrackContentProvider.Schema;
 import me.guillaumin.android.osmtracker.db.WaypointListAdapter;
 import android.app.ListActivity;
+import android.content.ContentResolver;
 import android.database.Cursor;
+import android.database.CursorWrapper;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.view.View;
 import android.widget.CursorAdapter;
+import android.widget.ListView;
 
 /**
  * Activity that lists the previous waypoints tracked by the user.
@@ -25,6 +33,35 @@ public class WaypointList extends ListActivity {
 		setListAdapter(new WaypointListAdapter(WaypointList.this, cursor));
 		
 		super.onResume();
+	}
+	
+	@Override
+	protected void onListItemClick(ListView lv, View v, int pos, long id) {
+		System.out.println("on list item click ");
+
+		/*		
+		 * TODO:
+		 * 
+		WaypointListAdapter wpa = (WaypointListAdapter)getListAdapter();
+		Cursor c = wpa.getCursor();
+
+		Object o = getListView().getItemAtPosition(pos);
+		System.out.println("debugitem "+o.getClass().getName());
+		CursorWrapper g = (CursorWrapper)o;
+		
+		if (wpa != null) {
+
+			String audioFile = getIntent().getExtras().getString(Schema.COL_LINK);
+			if (audioFile!=null && audioFile.endsWith(".3gpp")) {
+				Uri u = Uri.fromFile(new File(audioFile));
+				MediaPlayer player = MediaPlayer.create(this, u);
+				if (player != null) {
+					player.setLooping(false);
+					player.start();
+				}
+			}
+		}
+		*/
 	}
 
 	@Override
