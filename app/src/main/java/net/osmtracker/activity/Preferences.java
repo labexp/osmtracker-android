f2fb8efffc9181a1867a5ec530b1bf766f69c043
jamescr <jaime.cr@gmail.com>
2018-06-15 18:12:11 -0600

    support android 6 permission system - read external. Fix cancel button string.

--- app/src/main/java/net/osmtracker/activity/Preferences.java
+++ app/src/main/java/net/osmtracker/activity/Preferences.java
@@ -5,9 +5,12 @@ import java.io.FilenameFilter;
 
 import net.osmtracker.OSMTracker;
 import net.osmtracker.R;
+
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.preference.EditTextPreference;
@@ -18,6 +21,9 @@ import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 /**
  * Manages preferences screen.
@@ -29,6 +35,8 @@ public class Preferences extends PreferenceActivity {
 
 	@SuppressWarnings("unused")
 	private static final String TAG = Preferences.class.getSimpleName();
+
+	final private int RC_READ_PERMISSION = 1;
 	
 	/**
 	 * Directory containing user layouts, relative to storage dir.
@@ -72,14 +80,38 @@ public class Preferences extends PreferenceActivity {
 				preference.setSummary((String) newValue);
 				
 				// Re-populate layout list preference
-				populateLayoutPreference((String) newValue); 
+				populateLayoutPreference((String) newValue);
 				
 				// Set layout to default layout
 				((ListPreference) findPreference(OSMTracker.Preferences.KEY_UI_BUTTONS_LAYOUT)).setValue(OSMTracker.Preferences.VAL_UI_BUTTONS_LAYOUT);
 				return true;
 			}
 		});
-		populateLayoutPreference(storageDirPref.getText());
+
+		if (ContextCompat.checkSelfPermission(this,
+				Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+			// Should we show an explanation?
+			if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+					Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+				// Show an explanation to the user *asynchronously* -- don't block
+				// this thread waiting for the user's response! After the user
+				// sees the explanation, try again to request the permission.
+				// TODO: explain why we need permission.
+				Log.w(TAG, "we should explain why we need read permission");
+
+			} else {
+
+				// No explanation needed, we can request the permission.
+				ActivityCompat.requestPermissions(this,
+						new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, RC_READ_PERMISSION);
+			}
+
+		}  else {
+			populateLayoutPreference(storageDirPref.getText());
+		}
+
 
 		// Voice record duration
 		Preference pref = findPreference(OSMTracker.Preferences.KEY_VOICEREC_DURATION);
@@ -204,6 +236,29 @@ public class Preferences extends PreferenceActivity {
 		}
 		lf.setEntries(entries);
 		lf.setEntryValues(values);
+	}
+
+	public void onRequestPermissionsResult(int requestCode,
+										   String permissions[], int[] grantResults) {
+		switch (requestCode) {
+			case RC_READ_PERMISSION: {
+				// If request is cancelled, the result arrays are empty.
+				if (grantResults.length > 0
+						&& grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+					// permission was granted, yay!
+					EditTextPreference storageDirPref = (EditTextPreference) findPreference(OSMTracker.Preferences.KEY_STORAGE_DIR);
+					populateLayoutPreference(storageDirPref.getText());
+
+				} else {
+
+					// permission denied, boo! Disable the
+					// functionality that depends on this permission.
+					//TODO: add an informative message.
+					Log.w(TAG, "we should explain why we need read permission");
+				}
+			}
+		}
 	}
 	
 }
