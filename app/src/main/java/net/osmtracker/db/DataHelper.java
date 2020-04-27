5c78f9496d3a42c1f7c81901f04de5c505cf4de9
isaac.mena07 <xsaco07@gmail.com>
2020-04-26 19:23:21 -0600

    Use saved filename in the shared one

--- app/src/main/java/net/osmtracker/db/DataHelper.java
+++ app/src/main/java/net/osmtracker/db/DataHelper.java
@@ -386,16 +386,8 @@ public class DataHelper {
 	}
 
 	public static File getGPXTrackFile(long trackId, ContentResolver contentResolver, Context context) {
-		Uri trackUri = ContentUris.withAppendedId(TrackContentProvider.CONTENT_URI_TRACK, trackId);
 
-		Cursor cursor = contentResolver.query(trackUri, null, null,
-				null, null);
-
-		String trackName = "";
-		if(cursor != null && cursor.moveToFirst()) {
-			trackName = cursor.getString(cursor.getColumnIndex(TrackContentProvider.Schema.COL_NAME));
-			cursor.close();
-		}
+		String trackName = getTrackNameInDB(trackId, contentResolver);
 
 		File sdRoot = Environment.getExternalStorageDirectory();
 
@@ -410,6 +402,19 @@ public class DataHelper {
 				trackName.trim() + DataHelper.EXTENSION_GPX;
 
 		return new File(completeGPXTrackPath);
+	}
+
+	public static String getTrackNameInDB(long trackId, ContentResolver contentResolver) {
+		String trackName = "";
+		Uri trackUri = ContentUris.withAppendedId(TrackContentProvider.CONTENT_URI_TRACK, trackId);
+		Cursor cursor = contentResolver.query(trackUri, null, null,
+				null, null);
+		if(cursor != null && cursor.moveToFirst()) {
+			trackName = cursor.getString(cursor.getColumnIndex(TrackContentProvider.Schema.COL_NAME));
+			cursor.close();
+		}
+
+		return trackName;
 	}
 
 }
