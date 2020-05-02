911b475c22ed45d59c7a20b9fd31808595d6c4f4
Emmanuel Alfaro Brenes <ealfarobrenes@hotmail.com>
2020-05-01 21:47:18 -0600

    Add unit test for refreshActivity method in ButtonsPresets

--- app/src/test/java/net/osmtracker/activity/ButtonsPresetsTest.java
+++ app/src/test/java/net/osmtracker/activity/ButtonsPresetsTest.java
@@ -2,9 +2,14 @@ package net.osmtracker.activity;
 
 import android.content.SharedPreferences;
 import android.widget.CheckBox;
+import android.widget.LinearLayout;
+
+import net.osmtracker.R;
 
 import org.junit.Test;
 import org.mockito.internal.matchers.Any;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.reflect.Whitebox;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -12,7 +17,9 @@ import java.util.Hashtable;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.doNothing;
 import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.verifyPrivate;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 public class ButtonsPresetsTest {
@@ -52,7 +59,7 @@ public class ButtonsPresetsTest {
     }
 
     @Test
-    public void selectLayoutTest() throws Exception{
+    public void selectLayoutTest() throws Exception {
         ButtonsPresets activity = new ButtonsPresets();
 
         String label = "SOME LABEL";
@@ -108,6 +115,50 @@ public class ButtonsPresetsTest {
         // Make sure the value in SharedPreferences is updated to match the just selected
         verify(mockEditor).putString("ui.buttons.layout", label);
         verify(mockEditor).commit();
+    }
 
+    @Test
+    @PrepareForTest(ButtonsPresets.class)
+    public void refreshActivityTest() {
+        try {
+            ButtonsPresets mockActivity = mock(ButtonsPresets.class);
+
+            Hashtable internalHash = new Hashtable<String,String>();
+            internalHash.put("foo", "bar");
+
+            Whitebox.setInternalState(mockActivity.getClass(), "layoutsFileNames", internalHash);
+
+
+            LinearLayout downloadedLayouts = mock(LinearLayout.class);
+            LinearLayout defaultSection = mock(LinearLayout.class);
+
+
+            when(mockActivity,"refreshActivity").thenCallRealMethod();
+
+
+            when(mockActivity,"findViewById", R.id.list_layouts).thenReturn(downloadedLayouts);
+            when(mockActivity,"findViewById",R.id.buttons_presets).thenReturn(defaultSection);
+
+            // Downgrade mockito dependency to 1.6.2 for this to work
+            doNothing().when(mockActivity, "listLayouts", downloadedLayouts);
+
+            mockActivity.refreshActivity();
+
+            internalHash = Whitebox.getInternalState(mockActivity.getClass(), "layoutsFileNames");
+
+            // Check the hashtable is reset
+            assertEquals(0, internalHash.size());
+
+            // Check calls listLayouts
+            verifyPrivate(mockActivity).invoke("listLayouts", downloadedLayouts);
+
+            // Check calls checkCurrentLayout
+            verifyPrivate(mockActivity).invoke("checkCurrentLayout", downloadedLayouts, defaultSection);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Error testing refresh activity");
+            fail();
+        }
     }
 }
