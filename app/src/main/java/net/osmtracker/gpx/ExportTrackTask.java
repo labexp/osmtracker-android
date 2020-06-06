8efb20445eb9e9cda10ce1ba57fd5ce6df28677b
aton1698 <brandon1698@outlook.es>
2020-06-06 16:32:16 -0600

    Adding the barometric pressure info in WPT's and using the baro tag instead pressure_hpa tag

--- app/src/main/java/net/osmtracker/gpx/ExportTrackTask.java
+++ app/src/main/java/net/osmtracker/gpx/ExportTrackTask.java
@@ -387,7 +387,7 @@ public abstract class ExportTrackTask  extends AsyncTask<Void, Long, Boolean> {
 			if (! c.isNull(c.getColumnIndex(TrackContentProvider.Schema.COL_ATMOSPHERIC_PRESSURE))) { //Checking if the database contains atmospheric_pressure data
 				double pressure = c.getDouble(c.getColumnIndex(TrackContentProvider.Schema.COL_ATMOSPHERIC_PRESSURE));
 				String pressure_formatted = String.format("%.1f", pressure);
-				buff += "\t\t\t\t\t" + "<pressure_hpa>" + pressure_formatted + "</pressure_hpa>" + "\n";
+				buff += "\t\t\t\t\t" + "<baro>" + pressure_formatted + "</baro>" + "\n";
 			}
 
 			if(! buff.equals("")) {
@@ -499,12 +499,23 @@ public abstract class ExportTrackTask  extends AsyncTask<Void, Long, Boolean> {
 				out.append("\t\t" + "<hdop>" + (c.getDouble(c.getColumnIndex(TrackContentProvider.Schema.COL_ACCURACY)) / OSMTracker.HDOP_APPROXIMATION_FACTOR) + "</hdop>" + "\n");
 			}
 
-			if (OSMTracker.Preferences.VAL_OUTPUT_COMPASS_EXTENSION.equals(compass) &&
-					! c.isNull(c.getColumnIndex(TrackContentProvider.Schema.COL_COMPASS))) {
-				out.append("\t\t<extensions>\n");
-				out.append("\t\t\t"+ "<compass>" + c.getDouble(c.getColumnIndex(TrackContentProvider.Schema.COL_COMPASS)) + "</compass>\n");
-				out.append("\t\t\t" + "<compass_accuracy>" + c.getInt(c.getColumnIndex(TrackContentProvider.Schema.COL_COMPASS_ACCURACY)) + "</compass_accuracy>" + "\n");
-				out.append("\t\t</extensions>\n");
+			String buff = "";
+
+			if(OSMTracker.Preferences.VAL_OUTPUT_COMPASS_EXTENSION.equals(compass) && !c.isNull(c.getColumnIndex(TrackContentProvider.Schema.COL_COMPASS))) {
+				buff += "\t\t\t\t\t" + "<compass>" + c.getDouble(c.getColumnIndex(TrackContentProvider.Schema.COL_COMPASS)) + "</compass>" + "\n";
+				buff += "\t\t\t\t\t" + "<compass_accuracy>" + c.getDouble(c.getColumnIndex(TrackContentProvider.Schema.COL_COMPASS_ACCURACY)) + "</compass_accuracy>" + "\n";
+			}
+
+			if (! c.isNull(c.getColumnIndex(TrackContentProvider.Schema.COL_ATMOSPHERIC_PRESSURE))) { //Checking if the database contains atmospheric_pressure data
+				double pressure = c.getDouble(c.getColumnIndex(TrackContentProvider.Schema.COL_ATMOSPHERIC_PRESSURE));
+				String pressure_formatted = String.format("%.1f", pressure);
+				buff += "\t\t\t\t\t" + "<baro>" + pressure_formatted + "</baro>" + "\n";
+			}
+
+			if(! buff.equals("")) {
+				out.append("\t\t\t\t" + "<extensions>\n");
+				out.append(buff);
+				out.append("\t\t\t\t" + "</extensions>\n");
 			}
 
 			out.append("\t" + "</wpt>" + "\n");
