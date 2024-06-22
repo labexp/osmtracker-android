c0a9cd70d23b18472899470f3d27013f3d7eeb0d
Jaime Gutiérrez Alfaro <jaime.cr@gmail.com>
2024-06-21 20:25:22 -0600

    Set produccion url for Osm API

--- app/src/main/java/net/osmtracker/osm/OpenStreetMapConstants.java
+++ app/src/main/java/net/osmtracker/osm/OpenStreetMapConstants.java
@@ -2,38 +2,22 @@ package net.osmtracker.osm;
 
 public class OpenStreetMapConstants {
 
-	private static final boolean DEV_MODE = true;
+	private static final boolean DEV_MODE = false;
 	private static final String OSM_API_URL_DEV = "https://master.apis.dev.openstreetmap.org";
 	private static final String OSM_API_URL_PROD = "https://www.openstreetmap.org";
 	private static final String OSM_API_URL = (DEV_MODE) ? OSM_API_URL_DEV : OSM_API_URL_PROD;
-	
+
 	public static class Api {
 
-		private static final String PATH = "/api/0.6";
+		public static final String OSM_API_URL_PATH = OSM_API_URL + "/api/0.6/";
 
-		public static final String OSM_API_URL_PATH = OSM_API_URL + PATH + "/";
-		
-		public static class Gpx {
-			public static final String CREATE = OSM_API_URL + PATH + "/gpx/create";
-			
-			public static class Parameters {
-				public static final String FILE = "file";
-				public static final String DESCRIPTION = "description";
-				public static final String TAGS = "tags";
-				public static final String VISIBILITY = "visibility";
-			}
-		}
 	}
 
 	public static class OAuth2 {
-		public static final String CLIENT_ID_PROD = ""; // TODO: agregar el client ID para producción.
+		public static final String CLIENT_ID_PROD = "6s8TuIQoPeq89ZWUFOXU7EZ-ZaCUVtUoNZFIKCMdU-E";
 		public static final String CLIENT_ID_DEV = "94Ht-oVBJ2spydzfk18s1RV2z7NS98SBwMfzSCqLQLE"; // DEV
 
 		public static final String CLIENT_ID = (DEV_MODE) ? CLIENT_ID_DEV : CLIENT_ID_PROD;
-
-		public static final String CLIENT_SECRET_PROD = ""; // TODO: agregar el client ID para producción.
-		public static final String CLIENT_SECRET_DEV = "yL0NNWXXz9ZZUwQlFhiuuLWKzrdjWu7H00KkXTrr";
-		public static final String CLIENT_SECRET = (DEV_MODE) ? CLIENT_SECRET_DEV : CLIENT_SECRET_PROD;
 
 		public static final String SCOPE = "write_gpx";
 		public static final String USER_AGENT = "OSMTracker for Android™";
