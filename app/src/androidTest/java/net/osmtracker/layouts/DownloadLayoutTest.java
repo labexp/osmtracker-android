916e10aaa15e63a915a7ca09f46c048780af5a26
Binnette <binnette@gmail.com>
2024-12-04 09:25:27 +0100

    Replace deprecated ActivityTestRule

--- app/src/androidTest/java/net/osmtracker/layouts/DownloadLayoutTest.java
+++ app/src/androidTest/java/net/osmtracker/layouts/DownloadLayoutTest.java
@@ -19,8 +19,9 @@ import android.Manifest;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
-import androidx.test.rule.ActivityTestRule;
 import androidx.test.rule.GrantPermissionRule;
 
 import net.osmtracker.OSMTracker;
@@ -29,6 +30,8 @@ import net.osmtracker.activity.TrackManager;
 import net.osmtracker.util.TestUtils;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -36,107 +39,115 @@ import java.util.Locale;
 
 public class DownloadLayoutTest {
 
-    private final int WAIT_VIEW_TIMEOUT = 5000;
+	private final int WAIT_VIEW_TIMEOUT = 5000;
 
-    @Rule
-    public GrantPermissionRule fineLocationPermission = GrantPermissionRule.grant(Manifest.permission.ACCESS_FINE_LOCATION);
-    @Rule
-    public GrantPermissionRule coarseLocationPermission = GrantPermissionRule.grant(Manifest.permission.ACCESS_COARSE_LOCATION);
-    @Rule
-    public GrantPermissionRule writeStoragePermission = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+	@Rule
+	public GrantPermissionRule fineLocationPermission = GrantPermissionRule.grant(Manifest.permission.ACCESS_FINE_LOCATION);
+	@Rule
+	public GrantPermissionRule coarseLocationPermission = GrantPermissionRule.grant(Manifest.permission.ACCESS_COARSE_LOCATION);
+	@Rule
+	public GrantPermissionRule writeStoragePermission = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
-    @Rule
-    public ActivityTestRule<TrackManager> mRule = new ActivityTestRule(TrackManager.class) {
-        @Override
-        protected void beforeActivityLaunched() {
-            // Skip cool intro
-            SharedPreferences dtPrefs = PreferenceManager
-                    .getDefaultSharedPreferences(getInstrumentation().getTargetContext());
-            dtPrefs.edit().putBoolean(OSMTracker.Preferences.KEY_DISPLAY_APP_INTRO, false).apply();
-        }
-    };
+	public ActivityScenario<TrackManager> activity;
 
-    @Test
-    public void downloadLayoutTest() {
-        deleteLayoutsDirectory();
+	@Before
+	public void setUp() {
+		// Skip cool intro
+		SharedPreferences dtPrefs = PreferenceManager
+				.getDefaultSharedPreferences(getInstrumentation().getTargetContext());
+		dtPrefs.edit().putBoolean(OSMTracker.Preferences.KEY_DISPLAY_APP_INTRO, false).apply();
+		// Launch activity
+		activity = ActivityScenario.launch(TrackManager.class);
+		activity.moveToState(Lifecycle.State.RESUMED);
+	}
 
-        TestUtils.setLayoutsTestingRepository();
+	@After
+	public void tearDown() {
+		activity.close();
+	}
 
-        String layoutName = "abc";
+	@Test
+	public void downloadLayoutTest() {
+		deleteLayoutsDirectory();
 
-        navigateToAvailableLayouts();
+		TestUtils.setLayoutsTestingRepository();
 
-        clickButtonsToDownloadLayout(layoutName);
+		String layoutName = "abc";
 
-        makePostDownloadAssertions(layoutName);
-    }
+		navigateToAvailableLayouts();
 
+		clickButtonsToDownloadLayout(layoutName);
 
-    public void deleteLayoutsDirectory(){
-        try {
-            FileUtils.deleteDirectory(TestUtils.getLayoutsDirectory());
-        }catch (Exception e){
-            e.printStackTrace();
-            fail();
-        }
-    }
+		makePostDownloadAssertions(layoutName);
+	}
 
 
-    /**
-     * Assuming being in TrackManager
-     */
-    public void navigateToAvailableLayouts() {
-        // Open options menu in the Action Bar
-        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-        // Click on "Settings" in this menu
-        onView(withText(TestUtils.getStringResource(R.string.menu_settings))).perform(click());
-        // Click on "Buttons presets" settings
-        onData(withTitleText(TestUtils.getStringResource(R.string.prefs_ui_buttons_layout))).perform(scrollTo(), click());
-        // Wait for "+" to be visible
-        onView(isRoot()).perform(waitForView(R.id.launch_available, WAIT_VIEW_TIMEOUT));
-        // Perform a click action on the "+" button
-        onView(withId(R.id.launch_available)).perform(click());
-    }
+	public void deleteLayoutsDirectory() {
+		try {
+			FileUtils.deleteDirectory(TestUtils.getLayoutsDirectory());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
 
 
-    /**
-     * Check the new layouts appears as a new option
-     * Select the layout and check its buttons are shown when tracking
-     * @param layoutName
-     */
-    private void makePostDownloadAssertions(String layoutName) {
-        Espresso.pressBack();
-
-        // Check the layout appears as a new option in AvailableLayouts
-        onView(withText(layoutName.toLowerCase())).check(matches(isDisplayed()));
-
-        // Select the layout
-        onView(withText(layoutName.toLowerCase())).perform(click());
-
-        // Go to TrackLogger
-        Espresso.pressBack();
-        Espresso.pressBack();
-        onView(withId(R.id.trackmgr_fab)).perform(click());
-
-        // Check the buttons are loaded correctly
-        String expectedButtonsLabels[] = new String[]{"A", "B", "C"};
-        for(String label : expectedButtonsLabels)
-            onView(withText(label)).check(matches(isDisplayed()));
-
-    }
+	/**
+	 * Assuming being in TrackManager
+	 */
+	public void navigateToAvailableLayouts() {
+		// Open options menu in the Action Bar
+		openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+		// Click on "Settings" in this menu
+		onView(withText(TestUtils.getStringResource(R.string.menu_settings))).perform(click());
+		// Click on "Buttons presets" settings
+		onData(withTitleText(TestUtils.getStringResource(R.string.prefs_ui_buttons_layout))).perform(scrollTo(), click());
+		// Wait for "+" to be visible
+		onView(isRoot()).perform(waitForView(R.id.launch_available, WAIT_VIEW_TIMEOUT));
+		// Perform a click action on the "+" button
+		onView(withId(R.id.launch_available)).perform(click());
+	}
 
 
-    private void clickButtonsToDownloadLayout(String layoutName) {
-        onView(withText(layoutName)).perform(click());
+	/**
+	 * Check the new layouts appears as a new option
+	 * Select the layout and check its buttons are shown when tracking
+	 *
+	 * @param layoutName layout name
+	 */
+	private void makePostDownloadAssertions(String layoutName) {
+		Espresso.pressBack();
 
-        // Catch languages available dialog that shows up when the cell phone is not in English
-        if (! Locale.getDefault().getLanguage().equalsIgnoreCase("en")) {
-            onView(withText("English")).perform(click());
-        }
+		// Check the layout appears as a new option in AvailableLayouts
+		onView(withText(layoutName.toLowerCase())).check(matches(isDisplayed()));
 
-        onView(withText(TestUtils.getStringResource(R.string.available_layouts_description_dialog_positive_confirmation))).
-                perform(click());
+		// Select the layout
+		onView(withText(layoutName.toLowerCase())).perform(click());
 
-        TestUtils.checkToastIsShownWith(TestUtils.getStringResource(R.string.available_layouts_successful_download));
-    }
+		// Go to TrackLogger
+		Espresso.pressBack();
+		Espresso.pressBack();
+		onView(withId(R.id.trackmgr_fab)).perform(click());
+
+		// Check the buttons are loaded correctly
+		String[] expectedButtonsLabels = new String[]{"A", "B", "C"};
+		for (String label : expectedButtonsLabels)
+			onView(withText(label)).check(matches(isDisplayed()));
+
+	}
+
+
+	private void clickButtonsToDownloadLayout(String layoutName) {
+		onView(withText(layoutName)).perform(click());
+
+		// Catch languages available dialog that shows up when the cell phone is not in English
+		if (!Locale.getDefault().getLanguage().equalsIgnoreCase("en")) {
+			onView(withText("English")).perform(click());
+		}
+
+		onView(withText(TestUtils.getStringResource(R.string.available_layouts_description_dialog_positive_confirmation))).
+				perform(click());
+
+		TestUtils.checkToastIsShownWith(TestUtils.getStringResource(R.string.available_layouts_successful_download));
+	}
 }
