eb542947e4cfa32e8c1bcc31d620d4fef12af81b
Andy Porras <andyporras6@gmail.com>
2025-02-04 09:59:34 -0600

    Fix: remove trailing separator in filename for export, share, and OSM upload

--- app/src/main/java/net/osmtracker/gpx/ExportToTempFileTask.java
+++ app/src/main/java/net/osmtracker/gpx/ExportToTempFileTask.java
@@ -1,12 +1,17 @@
 package net.osmtracker.gpx;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
 import android.database.Cursor;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import net.osmtracker.OSMTracker;
 import net.osmtracker.db.DataHelper;
+import net.osmtracker.db.TrackContentProvider;
 import net.osmtracker.exception.ExportTrackException;
 
 import java.io.File;
@@ -31,15 +36,69 @@ public abstract class ExportToTempFileTask extends ExportTrackTask {
 			String exportLabelName = PreferenceManager.getDefaultSharedPreferences(context).getString(
 					OSMTracker.Preferences.KEY_EXPORT_LABEL,	OSMTracker.Preferences.VAL_EXPORT_LABEL);
 			String trackName = new DataHelper(context).getTrackById(trackId).getName();
+			long date = new DataHelper(context).getTrackById(trackId).getStartDate();
+
+			String formattedTrackStartDate = DataHelper.FILENAME_FORMATTER.format(new Date(date));
 
 			// Create temporary file
-			tmpFile = new File(context.getCacheDir(), trackName + exportLabelName+".gpx");
-			Log.d(TAG, "Temporary file: " + tmpFile.getAbsolutePath());
-
+			String namefinal = createFile(trackName, formattedTrackStartDate, exportLabelName);
+			tmpFile = new File(context.getCacheDir(),namefinal+".gpx");
+			Log.d(TAG, "Temporary file: "+ tmpFile.getAbsolutePath());
 		} catch (Exception ioe) {
 			Log.e(TAG, "Could not create temporary file", ioe);
 			throw new IllegalStateException("Could not create temporary file", ioe);
 		}
+	}
+	//create temporary file
+	private String createFile(String sanitizedTrackName, String formattedTrackStartDate, String exportLabelName) throws IOException{
+		String result = "";
+		String desiredOutputFormat = PreferenceManager.getDefaultSharedPreferences(context).getString(
+				OSMTracker.Preferences.KEY_OUTPUT_FILENAME,
+				OSMTracker.Preferences.VAL_OUTPUT_FILENAME);
+
+		boolean thereIsTrackName = sanitizedTrackName != null && sanitizedTrackName.length() >= 1;
+
+		switch(desiredOutputFormat){
+			case OSMTracker.Preferences.VAL_OUTPUT_FILENAME_NAME:
+				if(thereIsTrackName)
+					result += sanitizedTrackName;
+				else
+					result += formattedTrackStartDate;
+				if(!(exportLabelName.equals("")))
+					result += "_"+ exportLabelName; // fallback case
+				break;
+			case OSMTracker.Preferences.VAL_OUTPUT_FILENAME_NAME_DATE:
+				if(thereIsTrackName)
+					if(sanitizedTrackName.equals(formattedTrackStartDate)) {
+						result += sanitizedTrackName;
+					}else{
+						result += sanitizedTrackName + "_"  + formattedTrackStartDate; // name is not equal
+					}
+				else
+					result += formattedTrackStartDate;
+				if(!(exportLabelName.equals("")))
+					result += "_"+ exportLabelName;
+				break;
+			case OSMTracker.Preferences.VAL_OUTPUT_FILENAME_DATE_NAME:
+				if(thereIsTrackName){
+					if(sanitizedTrackName.equals(formattedTrackStartDate)){
+						result += formattedTrackStartDate;
+					}else{
+						result += formattedTrackStartDate  + "_" + sanitizedTrackName;
+					}
+				}else{
+					result += formattedTrackStartDate;
+				}
+				if(!(exportLabelName.equals("")))
+					result += "_"+ exportLabelName;
+				break;
+			case OSMTracker.Preferences.VAL_OUTPUT_FILENAME_DATE:
+				result += formattedTrackStartDate;
+				if(!(exportLabelName.equals("")))
+					result += "_"+ exportLabelName;
+				break;
+		}
+		return result;
 	}
 
 	@Override
