c7899119716b0263b642c741f214bc2a8f684c7a
Alain Knaff <alain@knaff.lu>
2025-08-15 18:45:45 +0200

    Create multiple track segments when tracking is stopped and later resumed to avoid criss-crossing lines on map

--- app/src/main/java/net/osmtracker/db/DatabaseHelper.java
+++ app/src/main/java/net/osmtracker/db/DatabaseHelper.java
@@ -39,7 +39,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 		+ TrackContentProvider.Schema.COL_TIMESTAMP + " long not null,"
 		+ TrackContentProvider.Schema.COL_COMPASS + " double null,"
 		+ TrackContentProvider.Schema.COL_COMPASS_ACCURACY + " integer null,"
-		+ TrackContentProvider.Schema.COL_ATMOSPHERIC_PRESSURE + " double null" + ")";
+		+ TrackContentProvider.Schema.COL_ATMOSPHERIC_PRESSURE + " double null,"
+		+ TrackContentProvider.Schema.COL_SEG_ID + " integer not null default 0"
+		+ ")";
 
 	/**
 	 * SQL for creating index TRACKPOINT_idx (track id)
@@ -202,6 +204,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 			db.execSQL("alter table " + TrackContentProvider.Schema.TBL_WAYPOINT + " add column " + TrackContentProvider.Schema.COL_ATMOSPHERIC_PRESSURE + " double null");
 		case 17:
 			db.execSQL(SQL_CREATE_TABLE_NOTE);
+		case 18:
+			db.execSQL("alter table "+TrackContentProvider.Schema.TBL_TRACKPOINT + " add column " + TrackContentProvider.Schema.COL_SEG_ID + " integer default 0");
 		}
 	}
 
