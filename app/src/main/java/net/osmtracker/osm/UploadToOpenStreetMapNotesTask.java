b12a1f709058bd4b4436b9e33a01f9346e6f1bb6
Jaime Gutiérrez Alfaro <jaime.cr@gmail.com>
2026-01-20 18:41:07 -0600

    Open Street Map Notes Upload clases and add unit test

--- app/src/main/java/net/osmtracker/osm/UploadToOpenStreetMapNotesTask.java
+++ app/src/main/java/net/osmtracker/osm/UploadToOpenStreetMapNotesTask.java
@@ -1,184 +1,173 @@
 package net.osmtracker.osm;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.content.SharedPreferences.Editor;
-import android.os.AsyncTask;
-import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.preference.PreferenceManager;
 
 import net.osmtracker.OSMTracker;
 import net.osmtracker.R;
 import net.osmtracker.db.DataHelper;
 import net.osmtracker.util.DialogUtils;
 
+import java.lang.ref.WeakReference;
+
 import de.westnordost.osmapi.OsmConnection;
 import de.westnordost.osmapi.common.errors.OsmAuthorizationException;
 import de.westnordost.osmapi.common.errors.OsmBadUserInputException;
 import de.westnordost.osmapi.map.data.OsmLatLon;
-import de.westnordost.osmapi.notes.Note; // Note object
-import de.westnordost.osmapi.notes.NotesApi; // Api for uploading notes to OSM
-import de.westnordost.osmapi.map.data.LatLon; // Data type for location points, maybe I'll put it in the dialog file
+import de.westnordost.osmapi.notes.NotesApi;
 
 /**
  * Uploads a note to OpenStreetMap
  *
  * @author Most of the code was made by Nicolas Guillaumin, adapted by Jose Andrés Vargas Serrano
  */
-public class UploadToOpenStreetMapNotesTask extends AsyncTask<Void, Void, Void> {
+public class UploadToOpenStreetMapNotesTask {
 
     private static final String TAG = UploadToOpenStreetMapNotesTask.class.getSimpleName();
 
-    /** Upload progress dialog */
-    private ProgressDialog dialog;
+	// Result constants
+	private static final int RESULT_OK = 1;
+	private static final int RESULT_ERROR_INTERNAL = -1;
+	private static final int RESULT_ERROR_AUTH = -2;
+	private static final int RESULT_ERROR_OSM_USER = -3;
 
-    private final Activity activity;
+	private final WeakReference<Activity> activityRef;
     private final String accessToken;
 
+	//OSM Note data
 	private final long noteId;
-
-    /** Note text */
     private final String noteText;
-
-    /** Note longitude */
     private final double longitude;
-
-    /** Note latitude */
     private final double latitude;
 
-    /**
-     * Error message, or text of the response returned by OSM
-     * if the request completed
-     */
+	private AlertDialog progressDialog;
+    // Error message returned by OSM if the request completed
     private String errorMsg;
+	private int resultCode = RESULT_ERROR_INTERNAL;
 
-    /**
-     * Either the HTTP result code, or -1 for an internal error
-     */
-    private int resultCode = -1;
-    private final int authorizationErrorResultCode = -2;
-    private final int anotherErrorResultCode = -3;
-    private final int okResultCode = 1;
-
-    // Not using an activity yet
-    public UploadToOpenStreetMapNotesTask(Activity activity, String accessToken, long noteId,
+	public UploadToOpenStreetMapNotesTask(Activity activity, String accessToken, long noteId,
 										  String noteText, double latitude, double longitude) {
-        this.activity = activity;
-        this.accessToken = accessToken;
+		this.activityRef = new WeakReference<>(activity);
+		this.accessToken = accessToken;
 		this.noteId = noteId;
-        this.noteText = noteText;
-        this.longitude = longitude;
-        this.latitude = latitude;
-    }
+		this.noteText = noteText;
+		this.longitude = longitude;
+		this.latitude = latitude;
+	}
 
-    @Override
-    protected void onPreExecute() {
-        try {
-            // Display progress dialog
-            dialog = new ProgressDialog(activity);
-            dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-            dialog.setIndeterminate(true);
-            dialog.setTitle(R.string.osm_note_upload);
+	/**
+	 * Synchronous execution for use with ExecutorService.
+	 * Replaces the lifecycle of AsyncTask. */
+	public void run() {
+		final Activity activity = activityRef.get();
+		if (activity == null || activity.isFinishing()) return;
 
-            dialog.setCancelable(false);
-            dialog.show();
+		// 1. Prepare UI (Equivalent to onPreExecute)
+		activity.runOnUiThread(() -> progressDialog = createProgressDialog(activity));
 
-        } catch (Exception e) {
-            Log.e(TAG, "onPreExecute() failed", e);
-            errorMsg = e.getLocalizedMessage();
-            cancel(true);
-        }
-    }
+		// 2. Execute Network Logic (Equivalent to doInBackground)
+		OsmConnection osm = new OsmConnection(
+				OpenStreetMapConstants.Api.OSM_API_URL_PATH,
+				OpenStreetMapConstants.OAuth2.USER_AGENT,
+				accessToken);
 
-    @Override
-    protected void onPostExecute(Void result) {
-        switch (resultCode) {
-            case -1:
-                dialog.dismiss();
-                // Internal error, the request didn't start at all
-                DialogUtils.showErrorDialog(activity,
-                        activity.getResources().getString(R.string.osm_note_upload_error)
-                                + ": " + errorMsg);
-                break;
-            case okResultCode:
-                dialog.dismiss();
-				// Success ! Update database and close activity
-				DataHelper.setNoteUploadDate(noteId, System.currentTimeMillis(), activity.getContentResolver());
+		try {
+			new NotesApi(osm).create(new OsmLatLon(latitude, longitude), noteText);
+			resultCode = RESULT_OK;
+		} catch (OsmBadUserInputException e) {
+			Log.e(TAG, "Bad OSM user input or illegal argument", e);
+			errorMsg = e.getLocalizedMessage();
+			resultCode = RESULT_ERROR_OSM_USER;
+		} catch (OsmAuthorizationException oae) {
+			Log.e(TAG, "OSM Authorization error", oae);
+			errorMsg = oae.getLocalizedMessage();
+			resultCode = RESULT_ERROR_AUTH;
+		} catch (Exception e) {
+			Log.e(TAG, "Upload error", e);
+			errorMsg = e.getLocalizedMessage();
+			resultCode = RESULT_ERROR_INTERNAL;
+		}
 
-                new AlertDialog.Builder(activity)
-                    .setTitle(android.R.string.dialog_alert_title)
-                    .setIcon(android.R.drawable.ic_dialog_info)
-                    .setMessage(R.string.osm_upload_sucess)
-                    .setCancelable(true)
-                    .setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                            activity.finish();
-                        }
-                    }).create().show();
+		// 3. Handle Results (Equivalent to onPostExecute)
+		activity.runOnUiThread(() -> {
+			Activity act = activityRef.get();
+			if (act == null || act.isFinishing()) return;
 
-                break;
-            case authorizationErrorResultCode:
-                dialog.dismiss();
-                Log.e(TAG, "onPostExecute() authorization failed: " + errorMsg + " (" + resultCode + ")");
-                // Authorization issue. Provide a way to clear credentials
-                new AlertDialog.Builder(activity)
-                    .setTitle(android.R.string.dialog_alert_title)
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .setMessage(R.string.osm_note_upload_unauthorized)
-                    .setCancelable(true)
-                    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    })
-                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Editor editor = PreferenceManager.getDefaultSharedPreferences(activity).edit();
-                            editor.remove(OSMTracker.Preferences.KEY_OSM_OAUTH2_ACCESSTOKEN);
-                            editor.commit();
+			if (progressDialog != null && progressDialog.isShowing()) {
+				progressDialog.dismiss();
+			}
 
-                            dialog.dismiss();
-                        }
-                    }).create().show();
-                break;
+			handleResult(act);
+		});
+	}
 
-            default:
-                // Another error. Display OSM response
-                dialog.dismiss();
-                // Internal error, the request didn't start at all
-                Log.e(TAG, "onPostExecute() default failed: " + errorMsg + " (" + resultCode + ")");
-                DialogUtils.showErrorDialog(activity,
-                        activity.getResources().getString(R.string.osm_note_upload_error)
-                                + ": " + errorMsg);
-        }
-    }
+	private void handleResult(Activity activity) {
+		switch (resultCode) {
+			case RESULT_OK:
+				DataHelper.setNoteUploadDate(noteId,
+						System.currentTimeMillis(), activity.getContentResolver());
+				new AlertDialog.Builder(activity)
+						.setIcon(android.R.drawable.ic_dialog_info)
+						.setMessage(R.string.osm_upload_sucess)
+						.setPositiveButton(android.R.string.ok,
+								(d, w) -> activity.finish())
+						.show();
+				break;
 
-    @Override
-    protected Void doInBackground(Void... params) {
-        OsmConnection osm = new OsmConnection(OpenStreetMapConstants.Api.OSM_API_URL_PATH,
-            OpenStreetMapConstants.OAuth2.USER_AGENT, accessToken);
+			case RESULT_ERROR_AUTH:
+				showAuthErrorDialog(activity);
+				break;
 
-        try {
-            LatLon point = new OsmLatLon(latitude, longitude);
-            Note note = new NotesApi(osm).create(point, noteText);
-            resultCode = okResultCode;
-        } catch (/*IOException |*/  IllegalArgumentException | OsmBadUserInputException e) {
-            Log.d(TAG, e.getMessage());
-            resultCode = -1; //internal error.
-        } catch (OsmAuthorizationException oae) {
-            Log.d(TAG, "OsmAuthorizationException");
-            resultCode = authorizationErrorResultCode;
-        } catch (Exception e) {
-            Log.e(TAG, e.getMessage());
-            resultCode = anotherErrorResultCode;
-        }
-        return null;
-    }
+			default:
+				DialogUtils.showErrorDialog(activity,
+						activity.getString(R.string.osm_note_upload_error) + ": " + errorMsg);
+				break;
+		}
+	}
+
+	private AlertDialog createProgressDialog(Activity activity) {
+		LinearLayout layout = new LinearLayout(activity);
+		layout.setOrientation(LinearLayout.HORIZONTAL);
+		layout.setPadding(50, 50, 50, 50);
+		layout.setGravity(Gravity.CENTER_VERTICAL);
+
+		ProgressBar pb = new ProgressBar(activity);
+		pb.setIndeterminate(true);
+		layout.addView(pb);
+
+		TextView tv = new TextView(activity);
+		tv.setText(R.string.osm_note_upload);
+		tv.setPadding(40, 0, 0, 0);
+		layout.addView(tv);
+
+		AlertDialog progressDialog = new AlertDialog.Builder(activity)
+				.setView(layout).setCancelable(false).create();
+		progressDialog.show();
+		return progressDialog;
+	}
+
+	/**
+	 * Helper to show the specific authorization error dialog
+	 */
+	private void showAuthErrorDialog(Activity activity) {
+		new AlertDialog.Builder(activity)
+				.setTitle(android.R.string.dialog_alert_title)
+				.setIcon(android.R.drawable.ic_dialog_alert)
+				.setMessage(R.string.osm_note_upload_unauthorized)
+				.setCancelable(true)
+				.setNegativeButton(android.R.string.no, (d, w) -> d.dismiss())
+				.setPositiveButton(android.R.string.yes, (d, w) -> {
+					PreferenceManager.getDefaultSharedPreferences(activity).edit()
+						.remove(OSMTracker.Preferences.KEY_OSM_OAUTH2_ACCESSTOKEN).apply();
+					d.dismiss();
+				}).show();
+	}
 
 }
