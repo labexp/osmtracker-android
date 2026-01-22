4e2c374ce7012848184cae79bd83e835b51d6ee4
Jaime Gutiérrez Alfaro <jaime.cr@gmail.com>
2026-01-22 10:05:21 -0600

    Refactor(test): Replace PowerMock with Mockito

--- app/src/test/java/net/osmtracker/util/URLCreatorTest.java
+++ app/src/test/java/net/osmtracker/util/URLCreatorTest.java
@@ -1,83 +1,62 @@
 package net.osmtracker.util;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
-import static org.powermock.api.mockito.PowerMockito.mock;
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
-import static org.powermock.api.mockito.PowerMockito.when;
 
-import androidx.preference.PreferenceManager;
+import androidx.test.core.app.ApplicationProvider;
 
-
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(PreferenceManager.class)
-@PowerMockIgnore("jdk.internal.reflect.*")
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = 25)
 public class URLCreatorTest {
 
-    Context mockContext;
-    SharedPreferences mockPrefs;
+	private Context context;
 
-
-    public void setupMocks(){
-        // Create SharedPreferences mock
-        mockPrefs = mock(SharedPreferences.class);
-
-        UnitTestUtils.setLayoutsDefaultRepository(mockPrefs);
-
-        // Create PreferenceManager mock
-        mockContext = mock(Context.class);
-        mockStatic(PreferenceManager.class);
-        when(PreferenceManager.getDefaultSharedPreferences(mockContext)).thenReturn(mockPrefs);
-
-    }
+	@Before
+	public void setUp() {
+		context = ApplicationProvider.getApplicationContext();
+	}
 
     @Test
     public void createMetadataDirUrl() {
-        setupMocks();
-        String result = URLCreator.createMetadataDirUrl(mockContext);
-        String expected = "https://api.github.com/repos/labexp/osmtracker-android-layouts/contents/layouts/metadata?ref=master";
-        assertEquals(result, expected);
+		String result = URLCreator.createMetadataDirUrl(context);
+		String expected = "https://api.github.com/repos/labexp/osmtracker-android-layouts/contents/layouts/metadata?ref=master";
+		assertEquals(expected, result);
     }
 
     @Test
     public void createMetadataFileURL() {
-        setupMocks();
-        String result = URLCreator.createMetadataFileURL(mockContext, "transporte_publico");
+        String result = URLCreator.createMetadataFileURL(context, "transporte_publico");
         String expected = "https://raw.githubusercontent.com/labexp/osmtracker-android-layouts/master/layouts/metadata/transporte_publico.xml";
-        assertEquals(result, expected);
+		assertEquals(expected, result);
     }
 
     @Test
     public void createLayoutFileURL() {
-        setupMocks();
-        String result = URLCreator.createLayoutFileURL(mockContext, "hidrantes","es");
+        String result = URLCreator.createLayoutFileURL(context, "hidrantes","es");
         String expected = "https://raw.githubusercontent.com/labexp/osmtracker-android-layouts/master/layouts/hidrantes/es.xml";
-        assertEquals(result, expected);
+		assertEquals(expected, result);
 
     }
 
     @Test
     public void createIconsDirUrl() {
-        setupMocks();
-        String result = URLCreator.createIconsDirUrl(mockContext, "hidrantes");
+        String result = URLCreator.createIconsDirUrl(context, "hidrantes");
         String expected = "https://api.github.com/repos/labexp/osmtracker-android-layouts/contents/layouts/hidrantes/hidrantes_icons?ref=master";
-        assertEquals(result, expected);
+		assertEquals(expected, result);
     }
 
     @Test
     public void createTestURL() {
-        setupMocks();
         String result = URLCreator.createTestURL("labexp", "osmtracker-android-layouts", "master");
         String expected = "https://api.github.com/repos/labexp/osmtracker-android-layouts/contents/layouts/metadata?ref=master";
-        assertEquals(result, expected);
+		assertEquals(expected, result);
     }
 
 }
