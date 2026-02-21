7c4cf6e2c49259154987c629d9226c42c1e7ce7f
Jaime Gutiérrez Alfaro <jaime.cr@gmail.com>
2026-02-21 07:52:39 -0600

    Refactor: Changes in variable names to keep consistency

--- app/src/main/java/net/osmtracker/db/TrackContentProvider.java
+++ app/src/main/java/net/osmtracker/db/TrackContentProvider.java
@@ -97,7 +97,7 @@ public class TrackContentProvider extends ContentProvider {
 				"FROM "+Schema.TBL_TRACKPOINT+" " +
 				"WHERE "+Schema.TBL_TRACKPOINT+"."+Schema.COL_TRACK_ID+" " +
 				"= " + Schema.TBL_TRACK + "." + Schema.COL_ID + ") " +
-				"as " + Schema.COL_MAX_SEG_ID
+				"as " + Schema.COL_SEG_ID_MAX
 	};
 
 	/**
@@ -598,15 +598,13 @@ public class TrackContentProvider extends ContentProvider {
 		public static final String COL_COMPASS = "compass_heading";
 		public static final String COL_COMPASS_ACCURACY = "compass_accuracy";
 		public static final String COL_ATMOSPHERIC_PRESSURE = "atmospheric_pressure";
-
 		public static final String COL_SEG_ID = "segment_id";
 
 		// virtual colums that are used in some sqls but dont exist in database
 		public static final String COL_TRACKPOINT_COUNT = "tp_count";
 		public static final String COL_WAYPOINT_COUNT = "wp_count";
 		public static final String COL_NOTE_COUNT = "note_count";
-
-		public static final String COL_MAX_SEG_ID = "max_segment_id";
+		public static final String COL_SEG_ID_MAX = "segment_id_max";
 
 		// Codes for UriMatcher
 		public static final int URI_CODE_TRACK = 3;
