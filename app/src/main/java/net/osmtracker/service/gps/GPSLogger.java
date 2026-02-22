338860e98b61027470ebc1ee627b60305393ee8d
Jaime Gutiérrez Alfaro <jaime.cr@gmail.com>
2026-02-22 14:39:14 -0600

    Refactor: Move segment ID database logic from GPSLogger to DataHelper

--- app/src/main/java/net/osmtracker/service/gps/GPSLogger.java
+++ app/src/main/java/net/osmtracker/service/gps/GPSLogger.java
@@ -6,8 +6,6 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.content.ContentResolver;
-import android.content.ContentUris;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -26,14 +24,11 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 import androidx.preference.PreferenceManager;
 
-import android.database.Cursor;
-
 import net.osmtracker.OSMTracker;
 import net.osmtracker.R;
 import net.osmtracker.activity.TrackLogger;
 import net.osmtracker.db.DataHelper;
 import net.osmtracker.db.TrackContentProvider;
-import net.osmtracker.db.model.Track;
 import net.osmtracker.listener.PressureListener;
 import net.osmtracker.listener.SensorListener;
 
@@ -113,7 +108,6 @@ public class GPSLogger extends Service implements LocationListener {
 	 * sensor for atmospheric pressure
 	 */
 	private PressureListener pressureListener = new PressureListener();
-
 
 	/**
 	 * Receives Intent for way point and notes tracking, and stop/start logging.
@@ -324,31 +318,13 @@ public class GPSLogger extends Service implements LocationListener {
 		super.onDestroy();
 	}
 
-	private long getSegIdFor(long trackId) {
-		ContentResolver cr = getContentResolver();
-		try(Cursor cursor =
-		    cr.query(ContentUris.withAppendedId(TrackContentProvider.CONTENT_URI_TRACK, trackId),
-			     null, null, null, null)) {
-
-			if (! cursor.moveToFirst())	{
-				Log.v(TAG, "Track "+trackId+" not found");
-				return 0;  // <--- Early return ---
-			}
-
-			return Track
-				.build(trackId, cursor, cr, true)
-				.getMaxSegId();
-		}
-	}
-
 	/**
 	 * Start GPS tracking.
 	 */
 	private void startTracking(long trackId) {
 		currentTrackId = trackId;
-		currentSegmentId = getSegIdFor(trackId)+1;
-		Log.v(TAG, "Starting track logging for track #" + trackId +
-		      "/" + currentSegmentId);
+		currentSegmentId = DataHelper.getSegmentIdFor(trackId, getContentResolver()) + 1;
+		Log.v(TAG, "Starting track logging for track #" + trackId + " segment #" +"/" + currentSegmentId);
 		// Refresh notification with correct Track ID
 		NotificationManager nmgr = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 		nmgr.notify(NOTIFICATION_ID, getNotification());
