1f960399917131dc692c4cdebf7f0f96180d8735
Alain Knaff <alain@knaff.lu>
2026-03-01 16:51:17 +0100

    mere presence of logs seems to fix this. Could it be a timing issue?

--- app/src/androidTest/java/net/osmtracker/activity/PreferencesTest.java
+++ app/src/androidTest/java/net/osmtracker/activity/PreferencesTest.java
@@ -67,24 +67,24 @@ public class PreferencesTest {
 	public void testStorageDirectoryValidatesNonEmpty() {
 		Log.i(TAG, "[1] Entering Test 1");
 		String keyTitle = context.getString(R.string.prefs_storage_dir);
-		Log.i(TAG, "[2] Test 1");
+		// Log.i(TAG, "[2] Test 1");
 		String defaultValue = OSMTracker.Preferences.VAL_STORAGE_DIR;
-		Log.i(TAG, "[3] Test 1");
+		// Log.i(TAG, "[3] Test 1");
 
 		// Looks for storage directory preference
 		scrollToAndClick(keyTitle);
-		Log.i(TAG, "[4] Test 1");
-		
+		// Log.i(TAG, "[4] Test 1");
+
 		// Try to save an empty value
 		onView(withId(android.R.id.edit)).perform(clearText());
-		Log.i(TAG, "[5] Test 1");
+		// Log.i(TAG, "[5] Test 1");
 		onView(withText(android.R.string.ok)).perform(click());
-		Log.i(TAG, "[6] Test 1");
+		// Log.i(TAG, "[6] Test 1");
 
 		// Open the preference to verify the value in the list remains the default (unchanged)
 		onView(ViewMatchers.isAssignableFrom(RecyclerView.class))
 				.check(matches(hasDescendant(withText(defaultValue))));
-		Log.i(TAG, "[7] Test 1");
+		// Log.i(TAG, "[7] Test 1");
 	}
 
 	/**
